070e7901d040a5389830a26119b8daf29c29fa49
Ann Sellén <brevtillann@hotmail.com>
2020-06-04 11:55:29 +0200

    tentan

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,30 @@
 mongoose = require('mongoose');
 
 const listingSchema = new mongoose.Schema({
+    address: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    monthlyFee: {
+        type: Number,
+        required: true,
+    },
+    coordinate: {
+        lat: {
+            type: Number,
+        },
+        lng: {
+            type: Number,
+        }
+    }
 });
 
 const Listing = mongoose.model('Listing', listingSchema);
