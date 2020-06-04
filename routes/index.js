3fab514bc2d3a638d625f2fc048b97bfb4c860fd
Robin Davoust <davoust88@hotmail.com>
2020-06-04 22:00:12 +0200

    added list and getByID

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 const listing = require('./listing.js')
 
 router.get("/listings", listing.get)
+router.get("/listings", listing.getById)
 router.post("/listings", listing.post)
 router.patch("/listings", listing.patch)
 router.delete("/listings", listing.deleteById)
+
 
 module.exports = router
