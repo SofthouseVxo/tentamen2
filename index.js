42c5c4e602d127b4c93498d85f557f50406547e4
Oscar Thorin <thorinenterprises@gmail.com>
2020-06-04 20:06:30 +0200

    added some statuts codes to requests

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = 4000;
 const listingRoutes = express.Router();
-const createError = require("http-errors");
 
 let Listing = require("./listing.model");
 
@@ -26,14 +25,14 @@ listingRoutes.route("/").get(function (req, res) {
   var name = req.query.name;
   if (name) {
     Listing.find({ Location: name }, function (err, listing) {
-      res.json(listing);
+      res.status(200).json(listing);
     });
   } else {
     Listing.find(function (err, listings) {
       if (err) {
         console.log(err);
       } else {
-        res.json(listings);
+        res.status(200).json(listings);
         console.log();
       }
     });
@@ -57,15 +56,39 @@ listingRoutes.route("/").post(function (req, res) {
 listingRoutes.route("/:id").get(function (req, res, next) {
   let id = req.params.id;
   Listing.findById(id, function (err, listing) {
-    res.json(listing);
+    res.status(200).json(listing);
   });
 });
 
 //DELETE Listing by id
 listingRoutes.route("/:id").delete((req, res) => {
   Listing.findByIdAndDelete(req.params.id)
-    .then(() => res.json("Deleted"))
-    .catch((err) => res.status(404).json(err));
+    .then(() => res.status(200).send("Deleted"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+listingRoutes.route("/:id").put(function (req, res) {
+  Listing.findById(req.params.id, function (err, listing) {
+    if (!listing) res.status(404).send(err);
+    else {
+      listing.Address = req.body.Address;
+      listing.Location = req.body.Location;
+      listing.Price = req.body.Price;
+      listing.MonthlyFee = req.body.MonthlyFee;
+      listing.Type = req.body.Type;
+      listing.Coordinate.Longitude = req.body.Coordinate.Longitude;
+      listing.Coordinate.Latitude = req.body.Coordinate.Latitude;
+
+      listing
+        .save()
+        .then((listing) => {
+          res.status(200).json(listing);
+        })
+        .catch((err) => {
+          next(err);
+        });
+    }
+  });
 });
 
 //PUT updates listing
@@ -81,21 +104,28 @@ listingRoutes.route("/:id").put(function (req, res) {
       listing.Coordinate.Longitude = req.body.Coordinate.Longitude;
       listing.Coordinate.Latitude = req.body.Coordinate.Latitude;
 
-      listing.save().then((listing) => {
-        res.json(listing);
-      });
+      listing
+        .save()
+        .then((listing) => {
+          res.status(200).json(listing);
+        })
+        .catch((err) => {
+          next(err);
+        });
     }
   });
 });
 
 app.use("/listings", listingRoutes);
 
-//404 Not Found handler, => to Error Handler
+//If path doesnt exist
 app.use((req, res, next) => {
-  next(createError(404, "Not Found"));
+  const err = new Error("Path Doesn't Exist");
+  err.status = 404;
+  next(err);
 });
 
-//Error Handler, Gives error 500 or the passed error object into Error Handler
+//Custom Error Handler
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.send({
